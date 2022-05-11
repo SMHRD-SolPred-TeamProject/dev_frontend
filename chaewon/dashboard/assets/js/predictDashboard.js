c5ab60d9c234c3fbba692712a3005c6dfc742f6e
전채원 <100102759+chaewon6400@users.noreply.github.com>
2022-05-11 16:37:06 +0900

    2

---
+++ chaewon/dashboard/assets/js/predictDashboard.js
@@ -0,0 +1,188 @@
+let realTimeGerateDom = document.getElementById("realtimeAOD");
+
+Highcharts.chart("predContainer", {
+  chart: {
+    type: "spline",
+    animation: Highcharts.svg, // don't animate in old IE
+    marginRight: 5,
+    events: {
+      load: function () {
+        // set up the updating of the chart each second
+        var series = this.series[0];
+        setInterval(function () {
+          var x = new Date().getTime(); // current time
+          let y = Math.random() * 600;
+
+          // 10초 단위로 바꿈!
+          realTimeGerateDom.innerText = y;
+          series.addPoint([x, y], true, true);
+        }, 10000);
+      },
+    },
+  },
+
+  time: {
+    useUTC: false,
+  },
+  title: {
+    text: "",
+  },
+
+  accessibility: {
+    announceNewData: {
+      enabled: true,
+      minAnnounceInterval: 15000,
+      announcementFormatter: function (allSeries, newSeries, newPoint) {
+        if (newPoint) {
+          return "New point added. Value: " + newPoint.y;
+        }
+        return false;
+      },
+    },
+  },
+
+  xAxis: {
+    type: "datetime",
+    tickPixelInterval: 150,
+  },
+
+  yAxis: {
+    title: {
+      text: "실시간 발전량",
+    },
+    plotLines: [
+      {
+        value: 0,
+        width: 1,
+        color: "#808080",
+      },
+    ],
+  },
+
+  tooltip: {
+    headerFormat: "<b>{series.name}</b><br/>",
+    pointFormat: "{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}",
+  },
+
+  legend: {
+    enabled: false,
+  },
+
+  exporting: {
+    enabled: false,
+  },
+
+  series: [
+    {
+      name: "real-time AOD",
+      data: (function () {
+        // generate an array of random data
+        var data = [],
+          time = new Date().getTime(),
+          i;
+
+        for (i = -19; i <= 0; i += 1) {
+          data.push({
+            x: time + i * 10000,
+            y: Math.random() * 600,
+          });
+        }
+        return data;
+      })(),
+    },
+  ],
+});
+
+// 차트2번
+Highcharts.chart("predContainer2", {
+  chart: {
+    type: "area",
+    animation: Highcharts.svg, // don't animate in old IE
+    marginRight: 5,
+    events: {
+      load: function () {
+        // set up the updating of the chart each second
+        var series = this.series[0];
+        setInterval(function () {
+          var x = new Date().getTime(); // current time
+          let y = Math.random() * 3000;
+
+          // 10초 단위로 바꿈!
+          realTimeGerateDom.innerText = y;
+          series.addPoint([x, y], true, true);
+        }, 10000);
+      },
+    },
+  },
+
+  time: {
+    useUTC: false,
+  },
+  title: {
+    text: "",
+  },
+
+  accessibility: {
+    announceNewData: {
+      enabled: true,
+      minAnnounceInterval: 15000,
+      announcementFormatter: function (allSeries, newSeries, newPoint) {
+        if (newPoint) {
+          return "New point added. Value: " + newPoint.y;
+        }
+        return false;
+      },
+    },
+  },
+
+  xAxis: {
+    type: "datetime",
+    tickPixelInterval: 150,
+  },
+
+  yAxis: {
+    title: {
+      text: "실시간 발전량",
+    },
+    plotLines: [
+      {
+        value: 0,
+        width: 1,
+        color: "#808080",
+      },
+    ],
+  },
+
+  tooltip: {
+    headerFormat: "<b>{series.name}</b><br/>",
+    pointFormat: "{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}",
+  },
+
+  legend: {
+    enabled: false,
+  },
+
+  exporting: {
+    enabled: false,
+  },
+
+  series: [
+    {
+      name: "real-time AOD",
+      data: (function () {
+        // generate an array of random data
+        var data = [],
+          time = new Date().getTime(),
+          i;
+
+        for (i = -19; i <= 0; i += 1) {
+          data.push({
+            x: time + i * 10000,
+            y: Math.random() * 3000,
+          });
+        }
+        return data;
+      })(),
+    },
+  ],
+});
