b0a0e79d179951ef861cf89b679778d0169724d1
전채원 <100102759+chaewon6400@users.noreply.github.com>
2022-05-12 11:41:58 +0900

    create solutionpage

---
+++ chaewon/dashboard/assets/js/core/cw.js
@@ -0,0 +1,12 @@
+const content = "ㅤNCT DREAM 앨범을 사보는 건 어떨까요?";
+const text = document.querySelector(".text");
+let i = 0;
+
+function typing() {
+  if (i < content.length) {
+    let txt = content.charAt(i);
+    text.innerHTML += txt;
+    i++;
+  }
+}
+setInterval(typing, 150);
