7e1a65fe1c8916211bf8add69312c6f7a6f93b91
전채원 <100102759+chaewon6400@users.noreply.github.com>
2022-05-16 10:42:05 +0900

    0516 다시시작

--- chaewon/dashboard/assets/js/todayDashboard.js
+++ chaewon/dashboard/assets/js/todayDashboard.js
@@ -1,6 +1,7 @@
 let realTimeGerateDom = document.getElementById("realtimeAOD");
-let integratedAOD = document.getElementById("integratedAOD");
+let realTimeTotalAOD = document.getElementById("integratedAOD");
 
+// 차트 1번 (실시간발전량)
 Highcharts.chart("container", {
   chart: {
     type: "spline",
@@ -94,7 +95,7 @@ Highcharts.chart("container", {
   ],
 });
 
-// 차트2번
+// 차트 2번 (실시간 누적발전량)
 Highcharts.chart("container2", {
   chart: {
     type: "area",
@@ -109,7 +110,106 @@ Highcharts.chart("container2", {
           let y = Math.random() * 3000;
 
           // 10초 단위로 바꿈!
-          integratedAOD.innerText = y;
+          realTimeTotalAOD.innerText = y;
+          series.addPoint([x, y], true, true);
+        }, 10000);
+      },
+    },
+  },
+
+  time: {
+    useUTC: false,
+  },
+  title: {
+    text: "",
+  },
+
+  accessibility: {
+    announceNewData: {
+      enabled: true,
+      minAnnounceInterval: 15000,
+      announcementFormatter: function (allSeries, newSeries, newPoint) {
+        if (newPoint) {
+          return "New point added. Value: " + newPoint.y;
+        }
+        return false;
+      },
+    },
+  },
+
+  xAxis: {
+    type: "datetime",
+    tickPixelInterval: 150,
+  },
+
+  yAxis: {
+    title: {
+      text: "실시간 발전량",
+    },
+    plotLines: [
+      {
+        value: 0,
+        width: 1,
+        color: "#808080",
+      },
+    ],
+  },
+
+  tooltip: {
+    headerFormat: "<b>{series.name}</b><br/>",
+    pointFormat: "{point.x:%Y-%m-%d %H:%M:%S}<br/>{point.y:.2f}",
+  },
+
+  legend: {
+    enabled: false,
+  },
+
+  exporting: {
+    enabled: false,
+  },
+
+  series: [
+    {
+      name: "real-time AOD",
+      data: (function () {
+        // generate an array of random data
+        var data = [],
+          time = new Date().getTime(),
+          i;
+
+        for (i = -19; i <= 0; i += 1) {
+          data.push({
+            x: time + i * 10000,
+            y: Math.random() * 3000,
+          });
+        }
+        return data;
+      })(),
+    },
+  ],
+});
+
+// solution.html
+// 이거는 솔루션페이지!
+
+// solution페이지의 실시간누적발전량
+let sol_realTimeTotalAOD = document.getElementById("integratedAOD2");
+
+Highcharts.chart("container2", {
+  chart: {
+    type: "area",
+    animation: Highcharts.svg, // don't animate in old IE
+    marginRight: 5,
+    events: {
+      load: function () {
+        // set up the updating of the chart each second
+        var series = this.series[0];
+        setInterval(function () {
+          var x = new Date().getTime(); // current time
+          let y = Math.random() * 3000;
+
+          // 10초 단위로 바꿈!
+          sol_realTimeTotalAOD.innerText = y;
           series.addPoint([x, y], true, true);
         }, 10000);
       },
